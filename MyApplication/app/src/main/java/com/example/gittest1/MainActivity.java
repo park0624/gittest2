b5eccfb782117ab1b084a7980d12f3bf1bd210cb
park0624 <park0624@hanmail.net>
2016-03-04 08:54:24 +0900

    changes1

---
+++ MyApplication/app/src/main/java/com/example/gittest1/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.gittest1;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+import android.content.DialogInterface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView text;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button;
+
+
+        button = (Button) findViewById(R.id.button);
+        text = (TextView) findViewById(R.id.textView);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                text.setText("dfdfdf");
+                text.setText("sksksksk");
+                text.setText("123123");
+            }
+        });
+
+    }
+}
